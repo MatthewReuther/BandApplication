1a2493664523e4b12439ce93c615478533a49fbb
Matthew Reuther <matthew.reuther@gmail.com>
2015-03-26 14:02:40 -0500

    added login feature to firebase

--- app/login/login.js
+++ app/login/login.js
@@ -1,4 +1,4 @@
-angular.module('bandApp.login', ['ngRoute'])
+angular.module('bandApp.login', ['ngRoute', 'firebase'])
 
 // Declared route
 .config(['$routeProvider', function($routeProvider) {
@@ -10,6 +10,27 @@ angular.module('bandApp.login', ['ngRoute'])
   }])
 
 // Login controller
-.controller('LoginController', [function() {
+.controller('LoginController', ['$scope','$firebaseSimpleLogin', function($scope,$firebaseSimpleLogin) {
+  var firebaseObj = new Firebase("https://bandapplication.firebaseio.com/");
+  var loginObj = $firebaseSimpleLogin(firebaseObj);
 
+  $scope.SignIn = function(e) {
+      event.preventDefault();  // To prevent form refresh
+      var username = $scope.user.email;     //use these to bind login html data to this function
+      var password = $scope.user.password;
+      // Auth Logic
+        loginObj.$login('password', {
+          email: username,
+          password: password
+        })
+        .then(function(user) {
+          // Success callback
+          console.log('Authentication successful');
+        },
+        function(error) {
+          // Failure callback
+          console.log('Authentication failure');
+        });
+      }
 }]);
+
