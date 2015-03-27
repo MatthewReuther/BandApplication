cb3fd086bb5b149cb7c275192c9ca1bfeacfc50f
Matthew Reuther <matthew.reuther@gmail.com>
2015-03-26 20:18:38 -0500

    calling creatuser in signup function and console logging if its succesful or not

--- app/auth/register/register.js
+++ app/auth/register/register.js
@@ -1,20 +1,37 @@
 
-angular.module('bandApp.register', ['ngRoute'])
+angular
+  .module('bandApp.register', ['ngRoute', 'firebase'])
+
 
 // Declared route
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/register', {
-      templateUrl: 'auth/register/register.html',
-      controller: 'RegisterController'
-    });
-}])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/register', {
+        templateUrl: 'auth/register/register.html',
+        controller: 'RegisterController'
+      });
+  }])
 
 // Register controller
-.controller('RegisterController', ['$scope', function($scope) {
-  $scope.signUp = function() {
-    if(!$scope.regForm.$invalid) {
-      console.log('yo it works');
+  .controller('RegisterController', ['$scope','$firebaseAuth', function($scope,$firebaseAuth) {
+    var firebaseObj = new Firebase("https://bandapplication.firebaseio.com/");
+    var auth = $firebaseAuth(firebaseObj);
+    //gets email addresses and password from scope and createUser
+    $scope.signUp = function() {
+      if(!$scope.regForm.$invalid) {
+        //console.log('yo it works');
+        var email    = $scope.user.email;
+        var password = $scope.user.password;
+        if (email && password) {
+          auth.$createUser(email, password)
+            .then(function (){
+              //create user if successful
+              console.log('Created User Successfully');
+            }, function(error) {
+              // failed creating user
+              console.log(error);
+          });
+      }
     }
   };
 }]);
